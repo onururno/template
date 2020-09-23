952a19373857bba964f8a7cb445b6a5f20134090
Onur <onur-erbil@hotmail.de>
2020-09-23 06:01:55 +0200

    installed : install devServer

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,17 @@ module.exports = {
     filename: "bundled.js", //change the auto generate file name
     path: path.resolve(__dirname, "app"), //requires absolute path!
   },
+  devServer: {
+    before: function (app, server) {
+      server._watch("./app/**/*.html"); //refresh html file by saving
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true, //inject CSS and JS into browser's memory on the fly
+    port: 3000, //localhost:3000
+    host: "0.0.0.0", //allow devices on the same network able to access the web server
+  },
   mode: "development", // to get rid of the Warining msg in terminal
-  watch: true, //web pack is acitvely watching us if we refresh the js file (STRG + C to cancel the watching!!!)
+  // watch: true, //web pack is acitvely watching us if we refresh the js file (STRG + C to cancel the watching!!!)
   module: {
     rules: [
       {
