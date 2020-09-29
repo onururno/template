46dc68fbb29a948227842360ea65682211072c1a
Onur <onur-erbil@hotmail.de>
2020-09-29 17:26:18 +0200

    Completed the Testimonial section

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,35 @@
-const path = require("path"); //it will generate an absolute path to the correct folder
+const path = require('path')
 
 const postCSSPlugins = [
-  require("postcss-import"),
-  require("postcss-mixins"),
-  require("postcss-simple-vars"),
-  require("postcss-nested"),
-  require("autoprefixer"),
-];
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js", //path to our js file
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js", //change the auto generate file name
-    path: path.resolve(__dirname, "app"), //requires absolute path!
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
   },
   devServer: {
-    before: function (app, server) {
-      server._watch("./app/**/*.html"); //refresh html file by saving
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
     },
-    contentBase: path.join(__dirname, "app"),
-    hot: true, //inject CSS and JS into browser's memory on the fly
-    port: 3000, //localhost:3000
-    host: "0.0.0.0", //allow devices on the same network able to access the web server
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
   },
-  mode: "development", // to get rid of the Warining msg in terminal
-  // watch: true, //web pack is acitvely watching us if we refresh the js file (STRG + C to cancel the watching!!!)
+  mode: 'development',
   module: {
     rules: [
       {
-        test: /\.css$/i, //works only if it pass .css
-        use: [
-          "style-loader", //refresh the style
-          "css-loader", //refresh css
-          { loader: "postcss-loader", options: { plugins: postCSSPlugins } },
-        ],
-      },
-    ],
-  },
-};
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+      }
+    ]
+  }
+}
