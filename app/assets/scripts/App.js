0634ae764fcff46dd7d4a5978d44fb2bfd185dd6
Onur <onur-erbil@hotmail.de>
2020-10-04 15:55:33 +0200

    Completed lazy loading and code splitting

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,32 @@
-import '../styles/styles.css'
-import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader'
-import Modal from './modules/Modal'
+import "../styles/styles.css";
+import "lazysizes";
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
-new Modal()
-let stickyHeader = new StickyHeader()
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+//seperate file for html which triggers by clicking
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was a problem."));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,40 +1,37 @@
 class Modal {
   constructor() {
-    this.injectHTML()
-    this.modal = document.querySelector(".modal")
-    this.closeIcon = document.querySelector(".modal__close")
-    this.openModalButtons = document.querySelectorAll(".open-modal")
-    this.events()
+    this.injectHTML();
+    this.modal = document.querySelector(".modal");
+    this.closeIcon = document.querySelector(".modal__close");
+    this.events();
   }
 
   events() {
-    // listen for open click
-    this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
-
     // listen for close click
-    this.closeIcon.addEventListener("click", () => this.closeTheModal())
+    this.closeIcon.addEventListener("click", () => this.closeTheModal());
 
     // pushes any key
-    document.addEventListener("keyup", e => this.keyPressHandler(e))
+    document.addEventListener("keyup", (e) => this.keyPressHandler(e));
   }
 
   keyPressHandler(e) {
     if (e.keyCode == 27) {
-      this.closeTheModal()
+      this.closeTheModal();
     }
   }
 
-  openTheModal(e) {
-    e.preventDefault()
-    this.modal.classList.add("modal--is-visible")
+  openTheModal() {
+    this.modal.classList.add("modal--is-visible");
   }
 
   closeTheModal() {
-    this.modal.classList.remove("modal--is-visible")
+    this.modal.classList.remove("modal--is-visible");
   }
 
   injectHTML() {
-    document.body.insertAdjacentHTML('beforeend', `
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
     <div class="modal">
     <div class="modal__inner">
       <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
@@ -51,8 +48,9 @@ class Modal {
     </div>
     <div class="modal__close">X</div>
   </div>
-    `)
+    `
+    );
   }
 }
 
-export default Modal
+export default Modal;
