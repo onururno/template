79fe1849845e774399184cc5df0fa07d2704dd1e
Onur <onur-erbil@hotmail.de>
2020-10-06 18:13:50 +0200

    Completed Client Area

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,23 +1,27 @@
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
   const secretContent = `
-        <h3>Welcome to The Secret Area </h3>
-        <p>Here we can tell you that the sky is blue</p>
-    `;
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `
+  
+  let body
 
   if (event.body) {
-    body = JSON.parse(event.body);
+    body = JSON.parse(event.body)
   } else {
-    body = {};
+    body = {}
   }
 
   if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
-      body: secretContent,
-    });
+      body: secretContent
+    })
   } else {
     callback(null, {
-      statusCode: 401,
-    });
+      statusCode: 401
+    })
   }
-};
+
+  
+}
